8cde25ead89f77502ed96a427d5da1d6a5d68755
Lee Jae Yeon <bestfourteen14@gmail.com>
2023-07-15 10:37:05 +0900

    movieboard

---
+++ movie/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Movie, Staff
+
+# Create your serializers here.
+# class CommentSerializer(serializers.ModelSerializer):
+#     user = serializers.ReadOnlyField(source = 'user.nickname')
+#     class Meta:
+#         model = Comment
+#         fields = ['post', 'user', 'created_at', 'comment']
+#         read_only_fields = ['user']
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = ['name', 'role', 'image_url']
+
+class MovieSerializer(serializers.ModelSerializer):
+    staff = StaffSerializer(many=True, read_only=True)
+    class Meta:
+        model = Movie
+        fields = ['title_kor', 
+                    'title_eng',
+                    'poster_url',
+                    'rating_aud', 
+                    'rating_cri', 
+                    'rating_net', 
+                    'genre', 
+                    'showtimes', 
+                    'release_date', 
+                    'rate', 
+                    'summary', 
+                    'staff']
