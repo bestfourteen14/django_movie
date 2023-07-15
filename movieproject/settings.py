909b0425ba56588ac1471d4364bce65c94e2293b
Lee Jae Yeon <bestfourteen14@gmail.com>
2023-07-15 10:53:31 +0900

    sql

--- movieproject/settings.py
+++ movieproject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +80,21 @@ WSGI_APPLICATION = 'movieproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_movie',
+        'USER': 'root',
+        'PASSWORD': 'rycbar123',
+        'HOST': 'svc.sel4.cloudtype.app',
+        'PORT': '31458'
     }
 }
 
